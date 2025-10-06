ab15195f33b68a268fe1d78357933fe8a8c23275
itsanat0m <mikkelsen.c.adam@gmail.com>
2025-10-06 13:57:17 -0400

    initial commit, beginning file writing program

---
+++ layout.md
@@ -0,0 +1,9 @@
+# What we need
+- [ ] allow for user input
+- [ ] as user types, send to file
+- [ ] read md specials as they are written
+
+# TODO
+- [ ] get input
+- [ ] check for specials
+- [ ] write to file
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string fileName = "";
+
+    cout << "Enter file name: /n";
+    cin >> fileName;
+    fileName = fileName + ".md";
+
+}
